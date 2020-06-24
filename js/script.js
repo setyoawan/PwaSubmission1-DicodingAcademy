1b7b1d4dc9f7a06eaa302d6056a4b9043e0bbb81
setyoawan <setyoawan8@gmail.com>
2020-06-24 07:12:34 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,73 @@
+document.addEventListener("DOMContentLoaded", function() {      
+
+    const mobile = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(mobile);
+    navigate();
+   
+    function navigate() {
+      const req = new XMLHttpRequest();
+      req.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status != 200) return;
+             
+          document.querySelectorAll(".navbar, .sidenav").forEach(function(e) {
+            e.innerHTML = req.responseText;
+          });
+        }
+      };
+      req.open("GET", "nav.html", true);
+      req.send();
+    }
+
+    let page = window.location.hash.substr(1);
+    if (page == "") page = "home";
+    loadPage(page);
+    
+    function loadPage(page) {
+    const req = new XMLHttpRequest();
+    req.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          let content = document.querySelector("#body-content");
+          if (this.status == 200) {
+              content.innerHTML = req.responseText;
+          } else if (this.status == 404) {
+              content.innerHTML = "<p>not found</p>";
+          } else {
+              content.innerHTML = "<p>not found</p>";
+          }
+        }
+    };
+    req.open("GET", `pages/${page}.html`, true);
+    req.send();
+    }
+
+    function navigate() {
+        const req = new XMLHttpRequest();
+        req.onreadystatechange = function() {
+          if (this.readyState == 4) {
+            if (this.status != 200) return;
+                   
+            document.querySelectorAll(".navbar, .sidenav").forEach(function(e) {
+              e.innerHTML = req.responseText;
+            });
+                   
+            document.querySelectorAll(".sidenav a, .navbar a").forEach(function(e) {
+              e.addEventListener("click", function(event) {
+                
+                const sidenav = document.querySelector(".sidenav");
+                M.Sidenav.getInstance(sidenav).close();
+                       
+                page = event.target.getAttribute("href").substr(1);
+                loadPage(page);
+              });
+            });
+          }
+        };
+        req.open("GET", "nav.html", true);
+        req.send();
+      }
+  });
+ 
+  
+
+    
---
+++ service-worker.js
@@ -0,0 +1,68 @@
+const CACHE_NAME = "pwaawp-v1";
+let urlsToCache = [
+  "/",
+  "/nav.html",
+  "/index.html",
+  "/pages/home.html",
+  "/pages/about.html",
+  "/pages/portfolio.html",
+  "/pages/contact.html",
+  "/css/style.css",
+  "/css/materialize.min.css",
+  "/js/materialize.min.js",
+  "/js/script.js",
+  "https://fonts.googleapis.com/icon?family=Material+Icons",
+  "/icon.png",
+  "/icon512.png",
+  "/assets/img/courses/cisco.png",
+  "/assets/img/courses/dicoding.png",
+  "/assets/img/project/movie-sap.png",
+  "/assets/img/project/socah.png",
+  "/assets/img/project/bangkalan.png",
+  "/assets/img/bg-img3.jpg",
+  "/assets/img/profil.jpg",
+  "/assets/img/setyo.jpg",
+  "/assets/img/utm-min.png"
+];
+ 
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", function(event) {
+    event.respondWith(
+      caches
+        .match(event.request, { cacheName: CACHE_NAME })
+        .then(function(response) {
+          if (response) {
+            console.log("ServiceWorker: Gunakan aset dari cache: ", response.url);
+            return response;
+          }
+   
+          console.log(
+            "ServiceWorker: Memuat aset dari server: ",
+            event.request.url
+          );
+          return fetch(event.request);
+        })
+    );
+  });
+
+  self.addEventListener("activate", function(event) {
+    event.waitUntil(
+      caches.keys().then(function(cacheNames) {
+        return Promise.all(
+          cacheNames.map(function(cacheName) {
+            if (cacheName != CACHE_NAME) {
+              console.log("ServiceWorker: cache " + cacheName + " dihapus");
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+    );
+  });
